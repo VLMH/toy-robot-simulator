b0c02eac9f8bceb7c32ee6bbcee227579cc9535f
Victor Lam <victorebox@yahoo.com.hk>
2015-09-05 19:10:42 +0800

    bring robot to playground

--- lib/toy_robot/simulator.rb
+++ lib/toy_robot/simulator.rb
@@ -5,10 +5,16 @@ module ToyRobot
     def initialize(name='Toy Robot Simulator', size=5)
       @name = name
       @playground = ToyRobot::Playground.new(size)
+      @robot = ToyRobot::Robot.new
     end
 
     def playground_size
       @playground.size
+    end
+
+    def place!(x, y, f)
+      raise ArgumentError, 'invalid coordinate' unless @playground.valid_coordinate?(x, y)
+      raise ArgumentError, 'invalid position' unless @robot.set_position(x, y, f)
     end
   end
 end
--- spec/toy_robot/simulator_spec.rb
+++ spec/toy_robot/simulator_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe ToyRobot::Simulator do
     specify { expect(game.playground_size).to eq(default_size) }
   end
 
+  describe '#place!' do
+    context 'when provide valid position' do
+      specify { expect{game.place!(0, 0, 'n')}.not_to raise_error }
+    end
+
+    context 'when provide invalid coordinate' do
+      specify { expect{game.place!(-1, 0, 'n')}.to raise_error(ArgumentError, 'invalid coordinate') }
+    end
+
+    context 'when provide invalid position' do
+      specify { expect{game.place!(0, 0, 'x')}.to raise_error(ArgumentError, 'invalid position') }
+    end
+  end
+
 end
