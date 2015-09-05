38b6bce6d16f5a50917a71fbe7e56405b0d75565
Victor Lam <victorebox@yahoo.com.hk>
2015-09-05 17:26:14 +0800

    robot should able to set position

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe ToyRobot::Robot do
   describe '#name' do
     specify { expect(robot.name).to eq(default_robot_name) }
   end
+
+  describe '#set_position' do
+    specify { expect(robot.set_position(0, 0, 'n')).to be_truthy }
+  end
 end
