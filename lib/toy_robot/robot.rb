a6af853e70af755e21cbffd393e550acf10017e2
Victor Lam <victorebox@yahoo.com.hk>
2015-09-05 17:51:45 +0800

    validate position before set

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,5 +1,10 @@
 module ToyRobot
   class Robot
+    NORTH = 'n'
+    EAST  = 'e'
+    SOUTH = 's'
+    WEST  = 'w'
+
     attr_accessor :name
 
     def initialize(name='BB8')
@@ -8,11 +13,29 @@ module ToyRobot
     end
 
     def set_position(x, y, f)
+      if !x.is_a?(Integer) ||
+         !y.is_a?(Integer) ||
+         !(direction = identify_direction(f))
+         return false
+      end
+
       @position = {
         :x => x,
         :y => y,
-        :f => f,
+        :f => direction,
       }
+    end
+
+    private
+
+    def identify_direction(f)
+      case f.downcase
+        when 'n', 'north' then NORTH
+        when 'e', 'east'  then EAST
+        when 's', 'south' then SOUTH
+        when 'w', 'west'  then WEST
+        else nil
+      end
     end
   end
 end
