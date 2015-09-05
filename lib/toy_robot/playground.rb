812e77a9711bd0da41f7acc9df70c80c6a7bc5e2
Victor Lam <victorebox@yahoo.com.hk>
2015-09-05 18:32:09 +0800

    validate a coordinate in playground

--- lib/toy_robot/playground.rb
+++ lib/toy_robot/playground.rb
@@ -5,5 +5,11 @@ module ToyRobot
     def initialize(size)
       @size = size
     end
+
+    def valid_coordinate?(x, y)
+      return false if !x.is_a?(Integer) || !y.is_a?(Integer) # invalid type
+      return false if x < 0 || y < 0 || x > @size || y > @size # within range
+      true
+    end
   end
 end
--- spec/toy_robot/playground_spec.rb
+++ spec/toy_robot/playground_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe ToyRobot::Playground do
   describe '#size' do
     specify { expect(playground.size).to eq(default_size) }
   end
+
+  describe '#valid_coordinate?' do
+    context 'when valid position provided' do
+      specify { expect(playground.valid_coordinate?(0, 0)).to be_truthy }
+      specify { expect(playground.valid_coordinate?(1, 2)).to be_truthy }
+      specify { expect(playground.valid_coordinate?(default_size, default_size)).to be_truthy }
+    end
+
+    context 'when invalid position provided' do
+      specify { expect(playground.valid_coordinate?('0', 0)).to be_falsey }
+      specify { expect(playground.valid_coordinate?(0, '0')).to be_falsey }
+      specify { expect(playground.valid_coordinate?(-1, -1)).to be_falsey }
+      specify { expect(playground.valid_coordinate?(-1, 0)).to be_falsey }
+      specify { expect(playground.valid_coordinate?(0, -1)).to be_falsey }
+      specify { expect(playground.valid_coordinate?(default_size + 1, default_size)).to be_falsey }
+      specify { expect(playground.valid_coordinate?(default_size, default_size + 1)).to be_falsey }
+    end
+  end
 end
