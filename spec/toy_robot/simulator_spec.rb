948e124064dfae6393dc7ae8e3e4b8cd7caff0a0
Victor Lam <victorebox@yahoo.com.hk>
2015-09-05 16:21:25 +0800

    revise spec with default value; test retrieve playground size

--- spec/toy_robot/simulator_spec.rb
+++ spec/toy_robot/simulator_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
-RSpec.describe 'ToyRobot::Simulator' do
+RSpec.describe ToyRobot::Simulator do
 
   let(:game) { ToyRobot::Simulator.new }
 
-  specify { expect(game.name).to eq('Toy Robot Simulator') }
+  specify { expect(game.name).to eq(default_name) }
+  specify { expect(game.playground_size).to eq(default_size) }
 
 end
