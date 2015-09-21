ea00522685cd9756fe09109bef00b777f297e303
Victor Lam <victorebox@yahoo.com.hk>
2015-09-21 09:43:15 +0800

    validate position when placing robot on playground; raise error if position is not valid

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -14,13 +14,13 @@ RSpec.describe ToyRobot::Robot do
     specify { expect(robot.name).to eq(default_robot_name) }
   end
 
-  describe '#set_position' do
-    specify { expect(robot.set_position(0, 0, 'n')).to be_truthy }
+  describe '#set_position!' do
+    specify { expect(robot.set_position!(0, 0, 'n')).to be_truthy }
 
     context 'when invalid position' do
-      specify { expect(robot.set_position('a', 0, 'n')).to be_falsey }
-      specify { expect(robot.set_position(0, 'b', 'n')).to be_falsey }
-      specify { expect(robot.set_position(0, 0, 'x')).to be_falsey }
+      specify { expect{robot.set_position!('a', 0, 'n')}.to raise_error(InvalidPositionError, 'Position is not valid') }
+      specify { expect{robot.set_position!(0, 'b', 'n')}.to raise_error(InvalidPositionError, 'Position is not valid') }
+      specify { expect{robot.set_position!(0, 0, 'x')}.to raise_error(InvalidPositionError, 'Position is not valid') }
     end
   end
 
