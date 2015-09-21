30fc659f7ff0fe95bdfa67d9d915c520fd3637e4
Victor Lam <victorebox@yahoo.com.hk>
2015-09-21 09:52:39 +0800

    fix spec of update set_position => set_position!

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe ToyRobot::Robot do
   describe '#position?' do
     context 'when position is set' do
       it 'return true' do
-        robot.set_position(0, 0, 'n')
+        robot.set_position!(0, 0, 'n')
         expect(robot.position?).to be_truthy
       end
     end
@@ -43,7 +43,7 @@ RSpec.describe ToyRobot::Robot do
       f = 'n'
       position = {x: x, y: y, f: 'n'}
 
-      before { robot.set_position(x, y, f) }
+      before { robot.set_position!(x, y, f) }
 
       specify { expect(robot.position).to eq(position) }
     end
@@ -56,14 +56,14 @@ RSpec.describe ToyRobot::Robot do
   describe '#step_forward!' do
     shared_examples 'make a step forward' do |x, y, f, expected_position|
       it 'step forward' do
-        robot.set_position(x, y, f)
+        robot.set_position!(x, y, f)
         expect(robot.step_forward!).to eq(expected_position)
       end
     end
 
     shared_examples 'make a step forward on a one grid playground' do |f|
       it 'step forward' do
-        robot_on_one_grid_playground.set_position(0, 0, f)
+        robot_on_one_grid_playground.set_position!(0, 0, f)
         original_position = robot_on_one_grid_playground.position.dup
 
         expect{robot_on_one_grid_playground.step_forward!}.to raise_error(ReachedBoundaryError, 'Robot reached boundary')
@@ -107,14 +107,14 @@ RSpec.describe ToyRobot::Robot do
   describe '#step_backward!' do
     shared_examples 'make a step backward' do |x, y, f, expected_position|
       it 'step backward' do
-        robot.set_position(x, y, f)
+        robot.set_position!(x, y, f)
         expect(robot.step_backward!).to eq(expected_position)
       end
     end
 
     shared_examples 'make a step backward on a one grid playground' do |f|
       it 'step backward' do
-        robot_on_one_grid_playground.set_position(0, 0, f)
+        robot_on_one_grid_playground.set_position!(0, 0, f)
         original_position = robot_on_one_grid_playground.position.dup
 
         expect{robot_on_one_grid_playground.step_backward!}.to raise_error(ReachedBoundaryError, 'Robot reached boundary')
@@ -158,7 +158,7 @@ RSpec.describe ToyRobot::Robot do
   describe '#turn_left!' do
     shared_examples 'turn left' do |x, y, f, expected_f|
       it "turn left from #{f}" do
-        robot.set_position(x, y, f)
+        robot.set_position!(x, y, f)
         expect(robot.turn_left![:f]).to eq(expected_f)
       end
     end
@@ -180,7 +180,7 @@ RSpec.describe ToyRobot::Robot do
   describe '#turn_right' do
     shared_examples 'turn right' do |x, y, f, expected_f|
       it "turn right from #{f}" do
-        robot.set_position(x, y, f)
+        robot.set_position!(x, y, f)
         expect(robot.turn_right![:f]).to eq(expected_f)
       end
     end
